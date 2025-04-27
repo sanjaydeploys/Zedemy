fb0ef073e2c43d325a8ffcb4db34ae8b9e45786c
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-04-27 21:40:00 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
 import { VitePWA } from 'vite-plugin-pwa';
-
 export default defineConfig({
   plugins: [
     react(),
