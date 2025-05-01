1434a5efb257cc4bf0d65d9c93aa3fba986ed4e9
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-05-01 09:15:20 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,1 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // Adjust based on your file structure
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
