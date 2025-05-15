b9b46f77f715d752731e680234db222e3b671ae6
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-05-15 18:41:07 +0530

    Update index.jsx

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -4,17 +4,16 @@ import App from './App';
 import React, { lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// Lazy load ToastContainer
 const ToastContainer = lazy(() => import('react-toastify').then(module => ({
   default: module.ToastContainer,
 })));
 
-import 'react-toastify/dist/ReactToastify.css'; // Consider dynamic import for CSS if possible
+import 'react-toastify/dist/ReactToastify.css';
 
+// No hydration, as we're rendering SSR content directly
 const rootElement = document.getElementById('root');
-const appRoot = createRoot(rootElement);
 
-appRoot.render(
+createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
